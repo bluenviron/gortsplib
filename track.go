099a87cd11597e9a5e90aae64222f815edcbcd24
aler9 <46489434+aler9@users.noreply.github.com>
2020-10-03 23:46:11 +0200

    NewTrack*: fix control attribute

--- track.go
+++ track.go
@@ -45,6 +45,10 @@ func NewTrackH264(id int, sps []byte, pps []byte) (*Track, error) {
 						"sprop-parameter-sets=" + spropParameterSets + "; " +
 						"profile-level-id=" + profileLevelId,
 				},
+				{
+					Key:   "control",
+					Value: "trackID=" + strconv.FormatInt(int64(id), 10),
+				},
 			},
 		},
 	}, nil
@@ -94,6 +98,10 @@ func NewTrackAac(id int, config []byte) (*Track, error) {
 						"indexlength=3; " +
 						"indexdeltalength=3; " +
 						"config=" + hex.EncodeToString(config),
+				},
+				{
+					Key:   "control",
+					Value: "trackID=" + strconv.FormatInt(int64(id), 10),
 				},
 			},
 		},
