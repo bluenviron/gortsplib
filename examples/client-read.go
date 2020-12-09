04f3c79fe4b29098e0ac836f740e962d88b12e2a
aler9 <46489434+aler9@users.noreply.github.com>
2020-12-09 12:19:32 +0100

    simplify examples

--- examples/client-read.go
+++ examples/client-read.go
@@ -21,11 +21,8 @@ func main() {
 	defer conn.Close()
 
 	// read track frames
-	done := conn.ReadFrames(func(id int, typ gortsplib.StreamType, buf []byte) {
+	err = <-conn.ReadFrames(func(id int, typ gortsplib.StreamType, buf []byte) {
 		fmt.Printf("frame from track %d, type %v: %v\n", id, typ, buf)
 	})
-
-	// catch any error
-	err = <-done
 	panic(err)
 }
