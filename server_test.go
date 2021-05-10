3a3691e51cf88ffebbd5b13eafcc5199bfc587d8
aler9 <46489434+aler9@users.noreply.github.com>
2021-05-10 18:55:52 +0200

    remove gortsplib_test.go

--- server_test.go
+++ server_test.go
@@ -5,6 +5,8 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net"
+	"os"
+	"os/exec"
 	"strconv"
 	"sync"
 	"testing"
@@ -135,6 +137,51 @@ func (sh *testServerHandler) OnGetParameter(ctx *ServerHandlerOnGetParameterCtx)
 		return sh.onGetParameter(ctx)
 	}
 	return nil, fmt.Errorf("unimplemented")
+}
+
+type container struct {
+	name string
+}
+
+func newContainer(image string, name string, args []string) (*container, error) {
+	c := &container{
+		name: name,
+	}
+
+	exec.Command("docker", "kill", "gortsplib-test-"+name).Run()
+	exec.Command("docker", "wait", "gortsplib-test-"+name).Run()
+
+	cmd := []string{"docker", "run",
+		"--network=host",
+		"--name=gortsplib-test-" + name,
+		"gortsplib-test-" + image}
+	cmd = append(cmd, args...)
+	ecmd := exec.Command(cmd[0], cmd[1:]...)
+	ecmd.Stdout = nil
+	ecmd.Stderr = os.Stderr
+
+	err := ecmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	time.Sleep(1 * time.Second)
+
+	return c, nil
+}
+
+func (c *container) close() {
+	exec.Command("docker", "kill", "gortsplib-test-"+c.name).Run()
+	exec.Command("docker", "wait", "gortsplib-test-"+c.name).Run()
+	exec.Command("docker", "rm", "gortsplib-test-"+c.name).Run()
+}
+
+func (c *container) wait() int {
+	exec.Command("docker", "wait", "gortsplib-test-"+c.name).Run()
+	out, _ := exec.Command("docker", "inspect", "gortsplib-test-"+c.name,
+		"--format={{.State.ExitCode}}").Output()
+	code, _ := strconv.ParseInt(string(out[:len(out)-1]), 10, 64)
+	return int(code)
 }
 
 var serverCert = []byte(`-----BEGIN CERTIFICATE-----
