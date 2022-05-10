bluenviron/gortsplib

commit 1e612f24435d6bab0965dac6c7c3b47f7e694efd

server: fix reading with ffplay and stateless ipv6

pkg/base/url.go
@@ -3,6 +3,7 @@ package base
 import (
 	"fmt"
 	"net/url"
+	"strings"
 )
 
 // URL is a RTSP URL.
@@ -12,6 +13,9 @@ type URL url.URL
 
 // ParseURL parses a RTSP URL.
 func ParseURL(s string) (*URL, error) {
+	s = strings.ReplaceAll(s, "%25", "%")
+	s = strings.ReplaceAll(s, "%", "%25")
+
 	u, err := url.Parse(s)
 	if err != nil {
 		return nil, err

pkg/base/url_test.go
@@ -14,6 +14,30 @@ func mustParseURL(s string) *URL {
 	return u
 }
 
+func TestURLParse(t *testing.T) {
+	for _, ca := range []struct {
+		name string
+		enc  string
+		u    *URL
+	}{
+		{
+			"ipv6 stateless",
+			`rtsp://[fe80::a8f4:3219:f33e:a072%wl0]:8554/proxied`,
+			&URL{
+				Scheme: "rtsp",
+				Host:   "[fe80::a8f4:3219:f33e:a072%wl0]:8554",
+				Path:   "/proxied",
+			},
+		},
+	} {
+		t.Run(ca.name, func(t *testing.T) {
+			u, err := ParseURL(ca.enc)
+			require.NoError(t, err)
+			require.Equal(t, ca.u, u)
+		})
+	}
+}
+
 func TestURLParseErrors(t *testing.T) {
 	for _, ca := range []struct {
 		name string

commit 6fe7a316bad54e8dfb7be47321c87d014ea80fe6

restore support for cameras with percent sign in password, broken after 1e612f2

pkg/base/url.go
@@ -3,6 +3,7 @@ package base
 import (
 	"fmt"
 	"net/url"
+	"regexp"
 	"strings"
 )
 
@@ -11,10 +12,17 @@ import (
 // control attributes.
 type URL url.URL
 
+var escapeRegexp = regexp.MustCompile(`^(.+?)://(.*?)@(.*?)/(.*?)$`)
+
 // ParseURL parses a RTSP URL.
 func ParseURL(s string) (*URL, error) {
-	s = strings.ReplaceAll(s, "%25", "%")
-	s = strings.ReplaceAll(s, "%", "%25")
+	// https://github.com/golang/go/issues/30611
+	m := escapeRegexp.FindStringSubmatch(s)
+	if m != nil {
+		m[3] = strings.ReplaceAll(m[3], "%25", "%")
+		m[3] = strings.ReplaceAll(m[3], "%", "%25")
+		s = m[1] + "://" + m[2] + "@" + m[3] + "/" + m[4]
+	}
 
 	u, err := url.Parse(s)
 	if err != nil {

pkg/base/url_test.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -22,11 +23,12 @@ func TestURLParse(t *testing.T) {
 	}{
 		{
 			"ipv6 stateless",
-			`rtsp://[fe80::a8f4:3219:f33e:a072%wl0]:8554/proxied`,
+			`rtsp://user:pa%23ss@[fe80::a8f4:3219:f33e:a072%wl0]:8554/prox%23ied`,
 			&URL{
 				Scheme: "rtsp",
 				Host:   "[fe80::a8f4:3219:f33e:a072%wl0]:8554",
-				Path:   "/proxied",
+				Path:   "/prox#ied",
+				User:   url.UserPassword("user", "pa#ss"),
 			},
 		},
 	} {