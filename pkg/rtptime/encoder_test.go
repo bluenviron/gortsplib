1005b38ec53d3b64f6e8b8d090f04a2929e33685
Alessandro Ros <aler9.dev@gmail.com>
2023-03-18 16:45:21 +0100

    add benchmark (#207)

--- pkg/rtptime/encoder_test.go
+++ pkg/rtptime/encoder_test.go
@@ -2,6 +2,7 @@ package rtptime
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -11,4 +12,13 @@ func TestEncoder(t *testing.T) {
 
 	ts := e.Encode(0)
 	require.Equal(t, uint32(12345), ts)
+}
+
+func BenchmarkEncoder(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		func() {
+			d := NewEncoder(90000, 0)
+			d.Encode(200 * time.Second)
+		}()
+	}
 }
