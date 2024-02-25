e9f2dac9d87ac8224ec5ddaf3e6218e7881b0f8b
aler9 <46489434+aler9@users.noreply.github.com>
2024-02-25 23:16:11 +0100

    change rtptime.Encoder signature and initialize time to a random value when not provided

--- pkg/rtptime/encoder_test.go
+++ pkg/rtptime/encoder_test.go
@@ -7,8 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func uint32Ptr(v uint32) *uint32 {
+	return &v
+}
+
 func TestEncoder(t *testing.T) {
-	e := NewEncoder(90000, 12345)
+	e := &Encoder{
+		ClockRate:        90000,
+		InitialTimestamp: uint32Ptr(12345),
+	}
+	err := e.Initialize()
+	require.NoError(t, err)
 
 	ts := e.Encode(0)
 	require.Equal(t, uint32(12345), ts)
@@ -18,10 +27,13 @@ func TestEncoder(t *testing.T) {
 }
 
 func BenchmarkEncoder(b *testing.B) {
+	e := &Encoder{
+		ClockRate:        90000,
+		InitialTimestamp: uint32Ptr(12345),
+	}
+	e.Initialize() //nolint:errcheck
+
 	for i := 0; i < b.N; i++ {
-		func() {
-			d := NewEncoder(90000, 0)
-			d.Encode(200 * time.Second)
-		}()
+		e.Encode(200 * time.Second)
 	}
 }
