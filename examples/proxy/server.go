982576a0715f5dd8cddf79f7646250fa78e415b6
Roger Hardiman <software@rjh.org.uk>
2024-08-04 11:44:34 +0100

    Emit the MULaw backchannel audio as UDP RTP to 127.0.0.1:9004

--- examples/proxy/server.go
+++ examples/proxy/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net"
 	"sync"
 
 	"github.com/bluenviron/gortsplib/v4"
@@ -110,7 +111,35 @@ func (s *server) OnPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Response, 
 	// assign the OnPacketRTPAny callback function so we can receive the backchannel audio
 	ctx.Session.OnPacketRTPAny(func(medi *description.Media, format format.Format, pkt *rtp.Packet) {
 		log.Printf("Got %s RTP data from VMS at %d size %d", format.Codec(), pkt.Timestamp, len(pkt.Payload))
-		//Extract audio data from RTP Payload and write to a file (or send to audio hardware on Raspberry Pi)
+
+		// Marshal the RTP Packet back into bytes and send via UDP
+		bytes, err := pkt.Marshal()
+		if err != nil {
+			return
+		}
+
+		// Send the RTP Payload from Port 9002 to destination port 127.0.0.1:9004 (note port numbers must be Even for RTP)
+		// Play the audio with
+		//     gst-launch-1.0 udpsrc port=9004 caps=application/x-rtp ! queue ! rtppcmudepay ! autoaudiosink sync=false
+		// or with
+		//     ffplay  -reorder_queue_size 0 rtp://0.0.0.0:9004          (use reorder_queue_size because if RTP packets stop then resume, FFMPEG complains RTP packets are too late)
+		//
+		conn, err := net.ListenPacket("udp", ":9002") // TODO - keep this open
+		if err != nil {
+			return
+		}
+
+		defer conn.Close()
+
+		dst, err := net.ResolveUDPAddr("udp", "127.0.0.1:9004")
+		if err != nil {
+			return
+		}
+
+		_, err = conn.WriteTo(bytes, dst)
+		if err != nil {
+			return
+		}
 	})
 
 	return &base.Response{
