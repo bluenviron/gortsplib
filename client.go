bluenviron/gortsplib

commit ead4471b5c7fd6ffd0ed52c3ef3de882667665b1

support RTSP-over-HTTP (#433) (#768)  (#887)

client.go
@@ -6,6 +6,7 @@ Examples are available at https://github.com/bluenviron/gortsplib/tree/main/exam
 package gortsplib
 
 import (
+	"bufio"
 	"context"
 	"crypto/rand"
 	"crypto/tls"
@@ -22,6 +23,7 @@ import (
 	"github.com/pion/rtcp"
 	"github.com/pion/rtp"
 
+	"github.com/bluenviron/gortsplib/v4/internal/asyncprocessor"
 	"github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
 	"github.com/bluenviron/gortsplib/v4/pkg/bytecounter"
@@ -460,15 +462,17 @@ type Client struct {
 	// a TLS configuration to connect to TLS (RTSPS) servers.
 	// It defaults to nil.
 	TLSConfig *tls.Config
+	// tunnel.
+	Tunnel Tunnel
+	// transport protocol (UDP, Multicast or TCP).
+	// If nil, it is chosen automatically (first UDP, then, if it fails, TCP).
+	// It defaults to nil.
+	Transport *TransportProtocol
 	// enable communication with servers which don't provide UDP server ports
 	// or use different server ports than the announced ones.
 	// This can be a security issue.
 	// It defaults to false.
 	AnyPortEnable bool
-	// transport protocol (UDP, Multicast or TCP).
-	// If nil, it is chosen automatically (first UDP, then, if it fails, TCP).
-	// It defaults to nil.
-	Transport *TransportProtocol
 	// If the client is reading with UDP, it must receive
 	// at least a packet within this timeout, otherwise it switches to TCP.
 	// It defaults to 3 seconds.
@@ -569,7 +573,7 @@ type Client struct {
 	keepAliveTimer       *time.Timer
 	closeError           error
 	writerMutex          sync.RWMutex
-	writer               *asyncProcessor
+	writer               *asyncprocessor.Processor
 	reader               *clientReader
 	timeDecoder          *rtptime.GlobalDecoder2
 	mustClose            bool
@@ -579,13 +583,17 @@ type Client struct {
 	bytesSent            *uint64
 
 	// in
-	chOptions  chan optionsReq
-	chDescribe chan describeReq
-	chAnnounce chan announceReq
-	chSetup    chan setupReq
-	chPlay     chan playReq
-	chRecord   chan recordReq
-	chPause    chan pauseReq
+	chOptions     chan optionsReq
+	chDescribe    chan describeReq
+	chAnnounce    chan announceReq
+	chSetup       chan setupReq
+	chPlay        chan playReq
+	chRecord      chan recordReq
+	chPause       chan pauseReq
+	chResponse    chan *base.Response
+	chRequest     chan *base.Request
+	chReadError   chan error
+	chWriterError chan error
 
 	// out
 	done chan struct{}
@@ -720,6 +728,10 @@ func (c *Client) Start2() error {
 	c.chPlay = make(chan playReq)
 	c.chRecord = make(chan recordReq)
 	c.chPause = make(chan pauseReq)
+	c.chResponse = make(chan *base.Response)
+	c.chRequest = make(chan *base.Request)
+	c.chReadError = make(chan error)
+	c.chWriterError = make(chan error)
 	c.done = make(chan struct{})
 
 	go c.run()
@@ -788,34 +800,6 @@ func (c *Client) run() {
 
 func (c *Client) runInner() error {
 	for {
-		chReaderResponse := func() chan *base.Response {
-			if c.reader != nil {
-				return c.reader.chResponse
-			}
-			return nil
-		}()
-
-		chReaderRequest := func() chan *base.Request {
-			if c.reader != nil {
-				return c.reader.chRequest
-			}
-			return nil
-		}()
-
-		chReaderError := func() chan error {
-			if c.reader != nil {
-				return c.reader.chError
-			}
-			return nil
-		}()
-
-		chWriterError := func() chan struct{} {
-			if c.writer != nil {
-				return c.writer.chStopped
-			}
-			return nil
-		}()
-
 		select {
 		case req := <-c.chOptions:
 			res, err := c.doOptions(req.url)
@@ -887,22 +871,22 @@ func (c *Client) runInner() error {
 			}
 			c.keepAliveTimer = time.NewTimer(c.keepAlivePeriod)
 
-		case <-chWriterError:
-			return c.writer.stopError
-
-		case err := <-chReaderError:
-			c.reader = nil
-			return err
-
-		case res := <-chReaderResponse:
+		case res := <-c.chResponse:
 			c.OnResponse(res)
 			// these are responses to keepalives, ignore them.
 
-		case req := <-chReaderRequest:
+		case req := <-c.chRequest:
 			err := c.handleServerRequest(req)
 			if err != nil {
 				return err
 			}
+
+		case err := <-c.chReadError:
+			c.reader = nil
+			return err
+
+		case err := <-c.chWriterError:
+			return err
 
 		case <-c.ctx.Done():
 			return liberrors.ErrClientTerminated{}
@@ -919,11 +903,13 @@ func (c *Client) waitResponse(requestCseqStr string) (*base.Response, error) {
 		case <-t.C:
 			return nil, liberrors.ErrClientRequestTimedOut{}
 
-		case err := <-c.reader.chError:
-			c.reader = nil
-			return nil, err
+		case req := <-c.chRequest:
+			err := c.handleServerRequest(req)
+			if err != nil {
+				return nil, err
+			}
 
-		case res := <-c.reader.chResponse:
+		case res := <-c.chResponse:
 			c.OnResponse(res)
 
 			// accept response if CSeq equals request CSeq, or if CSeq is not present
@@ -931,11 +917,9 @@ func (c *Client) waitResponse(requestCseqStr string) (*base.Response, error) {
 				return res, nil
 			}
 
-		case req := <-c.reader.chRequest:
-			err := c.handleServerRequest(req)
-			if err != nil {
-				return nil, err
-			}
+		case err := <-c.chReadError:
+			c.reader = nil
+			return nil, err
 
 		case <-c.ctx.Done():
 			return nil, liberrors.ErrClientTerminated{}
@@ -1133,8 +1117,8 @@ func (c *Client) stopTransportRoutines() {
 func (c *Client) createWriter() {
 	c.writerMutex.Lock()
 
-	c.writer = &asyncProcessor{
-		bufferSize: func() int {
+	c.writer = &asyncprocessor.Processor{
+		BufferSize: func() int {
 			if c.state == clientStateRecord || c.backChannelSetupped {
 				return c.WriteQueueSize
 			}
@@ -1144,19 +1128,25 @@ func (c *Client) createWriter() {
 			// decrease RAM consumption by allocating less buffers.
 			return 8
 		}(),
+		OnError: func(ctx context.Context, err error) {
+			select {
+			case <-ctx.Done():
+			case <-c.ctx.Done():
+			case c.chWriterError <- err:
+			}
+		},
 	}
-
-	c.writer.initialize()
+	c.writer.Initialize()
 
 	c.writerMutex.Unlock()
 }
 
 func (c *Client) startWriter() {
-	c.writer.start()
+	c.writer.Start()
 }
 
 func (c *Client) destroyWriter() {
-	c.writer.close()
+	c.writer.Close()
 
 	c.writerMutex.Lock()
 	c.writer = nil
@@ -1175,25 +1165,45 @@ func (c *Client) connOpen() error {
 	dialCtx, dialCtxCancel := context.WithTimeout(c.ctx, c.ReadTimeout)
 	defer dialCtxCancel()
 
-	nconn, err := c.DialContext(dialCtx, "tcp", canonicalAddr(&base.URL{
+	addr := canonicalAddr(&base.URL{
 		Scheme: c.Scheme,
 		Host:   c.Host,
-	}))
-	if err != nil {
-		return err
+	})
+
+	var tlsConfig *tls.Config
+	if c.Scheme == "rtsps" {
+		tlsConfig = c.TLSConfig
+		if tlsConfig == nil {
+			host, _, _ := net.SplitHostPort(addr)
+			tlsConfig = &tls.Config{
+				ServerName: host,
+			}
+		}
 	}
 
-	if c.Scheme == "rtsps" {
-		tlsConfig := c.TLSConfig
-		if tlsConfig == nil {
-			tlsConfig = &tls.Config{}
+	var nconn net.Conn
+
+	if c.Tunnel == TunnelHTTP {
+		var err error
+		nconn, err = newClientHTTPTunnel(dialCtx, c.DialContext, addr, tlsConfig)
+		if err != nil {
+			return err
 		}
-		nconn = tls.Client(nconn, tlsConfig)
+	} else {
+		var err error
+		nconn, err = c.DialContext(dialCtx, "tcp", addr)
+		if err != nil {
+			return err
+		}
+
+		if tlsConfig != nil {
+			nconn = tls.Client(nconn, tlsConfig)
+		}
 	}
 
 	c.nconn = nconn
 	bc := bytecounter.New(c.nconn, c.bytesReceived, c.bytesSent)
-	c.conn = conn.NewConn(bc)
+	c.conn = conn.NewConn(bufio.NewReader(bc), bc)
 	c.reader = &clientReader{
 		c: c,
 	}
@@ -1648,7 +1658,7 @@ func (c *Client) doSetup(
 			th.Profile = headers.TransportProfileAVP
 		}
 
-		if th.Profile == headers.TransportProfileSAVP || c.Scheme == "rtsp" {
+		if c.Tunnel == TunnelNone && (th.Profile == headers.TransportProfileSAVP || c.Scheme == "rtsp") {
 			protocol = TransportUDP
 		} else {
 			protocol = TransportTCP
@@ -2462,7 +2472,9 @@ func (c *Client) Transport2() *ClientTransport {
 	defer c.propsMutex.RUnlock()
 
 	return &ClientTransport{
-		Conn:    ConnTransport{},
+		Conn: ConnTransport{
+			Tunnel: c.Tunnel,
+		},
 		Session: c.setuppedTransport,
 	}
 }

client_reader.go
@@ -12,17 +12,9 @@ type clientReader struct {
 
 	mutex                  sync.Mutex
 	allowInterleavedFrames bool
-
-	chResponse chan *base.Response
-	chRequest  chan *base.Request
-	chError    chan error
 }
 
 func (r *clientReader) start() {
-	r.chResponse = make(chan *base.Response)
-	r.chRequest = make(chan *base.Request)
-	r.chError = make(chan error)
-
 	go r.run()
 }
 
@@ -35,17 +27,16 @@ func (r *clientReader) setAllowInterleavedFrames(v bool) {
 func (r *clientReader) wait() {
 	for {
 		select {
-		case <-r.chError:
+		case <-r.c.chResponse:
+		case <-r.c.chRequest:
+		case <-r.c.chReadError:
 			return
-
-		case <-r.chResponse:
-		case <-r.chRequest:
 		}
 	}
 }
 
 func (r *clientReader) run() {
-	r.chError <- r.runInner()
+	r.c.chReadError <- r.runInner()
 }
 
 func (r *clientReader) runInner() error {
@@ -57,10 +48,10 @@ func (r *clientReader) runInner() error {
 
 		switch what := what.(type) {
 		case *base.Response:
-			r.chResponse <- what
+			r.c.chResponse <- what
 
 		case *base.Request:
-			r.chRequest <- what
+			r.c.chRequest <- what
 
 		case *base.InterleavedFrame:
 			r.mutex.Lock()

server.go
@@ -3,6 +3,7 @@ package gortsplib
 import (
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"net"
 	"strconv"
@@ -18,6 +19,8 @@ const (
 	serverHeader    = "gortsplib"
 	serverAuthRealm = "ipcam"
 )
+
+var errHTTPUpgraded = errors.New("upgraded to HTTP conn")
 
 func extractPort(address string) (int, error) {
 	_, tmp, err := net.SplitHostPort(address)
@@ -45,6 +48,13 @@ type sessionRequestReq struct {
 	id     string
 	create bool
 	res    chan sessionRequestRes
+}
+
+type sessionHandleHTTPChannelReq struct {
+	sc       *ServerConn
+	write    bool
+	tunnelID string
+	res      chan error
 }
 
 type chGetMulticastIPReq struct {
@@ -129,25 +139,27 @@ type Server struct {
 	sessionTimeout       time.Duration
 	checkStreamPeriod    time.Duration
 
-	ctx             context.Context
-	ctxCancel       func()
-	wg              sync.WaitGroup
-	multicastNet    *net.IPNet
-	multicastNextIP net.IP
-	tcpListener     *serverTCPListener
-	udpRTPListener  *serverUDPListener
-	udpRTCPListener *serverUDPListener
-	sessions        map[string]*ServerSession
-	conns           map[*ServerConn]struct{}
-	closeError      error
+	ctx              context.Context
+	ctxCancel        func()
+	wg               sync.WaitGroup
+	multicastNet     *net.IPNet
+	multicastNextIP  net.IP
+	tcpListener      *serverTCPListener
+	udpRTPListener   *serverUDPListener
+	udpRTCPListener  *serverUDPListener
+	conns            map[*ServerConn]struct{}
+	httpReadChannels map[*ServerConn]chan error
+	sessions         map[string]*ServerSession
+	closeError       error
 
 	// in
-	chNewConn        chan net.Conn
-	chAcceptErr      chan error
-	chCloseConn      chan *ServerConn
-	chHandleRequest  chan sessionRequestReq
-	chCloseSession   chan *ServerSession
-	chGetMulticastIP chan chGetMulticastIPReq
+	chNewConn           chan net.Conn
+	chAcceptErr         chan error
+	chCloseConn         chan *ServerConn
+	chHandleHTTPChannel chan sessionHandleHTTPChannelReq
+	chHandleRequest     chan sessionRequestReq
+	chCloseSession      chan *ServerSession
+	chGetMulticastIP    chan chGetMulticastIPReq
 }
 
 // Start starts the server.
@@ -305,18 +317,18 @@ func (s *Server) Start() error {
 
 	s.ctx, s.ctxCancel = context.WithCancel(context.Background())
 
-	s.sessions = make(map[string]*ServerSession)
 	s.conns = make(map[*ServerConn]struct{})
+	s.httpReadChannels = make(map[*ServerConn]chan error)
+	s.sessions = make(map[string]*ServerSession)
 	s.chNewConn = make(chan net.Conn)
 	s.chAcceptErr = make(chan error)
 	s.chCloseConn = make(chan *ServerConn)
+	s.chHandleHTTPChannel = make(chan sessionHandleHTTPChannelReq)
 	s.chHandleRequest = make(chan sessionRequestReq)
 	s.chCloseSession = make(chan *ServerSession)
 	s.chGetMulticastIP = make(chan chGetMulticastIPReq)
 
-	s.tcpListener = &serverTCPListener{
-		s: s,
-	}
+	s.tcpListener = &serverTCPListener{s: s}
 	err := s.tcpListener.initialize()
 	if err != nil {
 		if s.udpRTPListener != nil {
@@ -355,6 +367,8 @@ func (s *Server) run() {
 
 	s.ctxCancel()
 
+	s.tcpListener.close()
+
 	if s.udpRTCPListener != nil {
 		s.udpRTCPListener.close()
 	}
@@ -362,8 +376,6 @@ func (s *Server) run() {
 	if s.udpRTPListener != nil {
 		s.udpRTPListener.close()
 	}
-
-	s.tcpListener.close()
 }
 
 func (s *Server) runInner() error {
@@ -385,10 +397,36 @@ func (s *Server) runInner() error {
 				continue
 			}
 			delete(s.conns, sc)
+			delete(s.httpReadChannels, sc)
 			sc.Close()
 
+		case req := <-s.chHandleHTTPChannel:
+			if !req.write {
+				req.sc.httpReadTunnelID = req.tunnelID
+				s.httpReadChannels[req.sc] = req.res
+			} else {
+				readChan, readChanRes := s.findHTTPReadChannel(req.sc, req.tunnelID)
+				if readChan == nil {
+					req.res <- fmt.Errorf("did not found a corresponding HTTP GET request")
+				} else {
+					delete(s.httpReadChannels, readChan)
+					close(readChanRes)
+					req.res <- errHTTPUpgraded
+
+					nconn := newServerHTTPTunnel(req.sc.nconn, req.sc.httpReadBuf, readChan.nconn)
+					sc := &ServerConn{
+						s:      s,
+						nconn:  nconn,
+						isHTTP: true,
+					}
+					sc.initialize()
+					s.conns[sc] = struct{}{}
+				}
+			}
+
 		case req := <-s.chHandleRequest:
-			if ss, ok := s.sessions[req.id]; ok {
+			ss, ok := s.sessions[req.id]
+			if ok {
 				if !req.sc.ip().Equal(ss.author.ip()) ||
 					req.sc.zone() != ss.author.zone() {
 					req.res <- sessionRequestRes{
@@ -398,17 +436,6 @@ func (s *Server) runInner() error {
 						err: liberrors.ErrServerCannotUseSessionCreatedByOtherIP{},
 					}
 					continue
-				}
-
-				select {
-				case ss.chHandleRequest <- req:
-				case <-ss.ctx.Done():
-					req.res <- sessionRequestRes{
-						res: &base.Response{
-							StatusCode: base.StatusBadRequest,
-						},
-						err: liberrors.ErrServerTerminated{},
-					}
 				}
 			} else {
 				if !req.create {
@@ -427,18 +454,9 @@ func (s *Server) runInner() error {
 				}
 				ss.initialize()
 				s.sessions[ss.secretID] = ss
-
-				select {
-				case ss.chHandleRequest <- req:
-				case <-ss.ctx.Done():
-					req.res <- sessionRequestRes{
-						res: &base.Response{
-							StatusCode: base.StatusBadRequest,
-						},
-						err: liberrors.ErrServerTerminated{},
-					}
-				}
 			}
+
+			ss.handleRequestNoWait(req)
 
 		case ss := <-s.chCloseSession:
 			if sss, ok := s.sessions[ss.secretID]; !ok || sss != ss {
@@ -476,6 +494,16 @@ func (s *Server) StartAndWait() error {
 	defer s.Close()
 
 	return s.Wait()
+}
+
+func (s *Server) findHTTPReadChannel(writeChan *ServerConn, tunnelID string) (*ServerConn, chan error) {
+	for readChan, readChanRes := range s.httpReadChannels {
+		if readChan.remoteAddr.IP.Equal(writeChan.remoteAddr.IP) &&
+			readChan.httpReadTunnelID == tunnelID {
+			return readChan, readChanRes
+		}
+	}
+	return nil, nil
 }
 
 func (s *Server) getMulticastIP() (net.IP, error) {
@@ -516,6 +544,34 @@ func (s *Server) closeSession(ss *ServerSession) {
 	case s.chCloseSession <- ss:
 	case <-s.ctx.Done():
 	}
+}
+
+func (s *Server) handleHTTPChannel(req sessionHandleHTTPChannelReq) error {
+	req.res = make(chan error)
+
+	select {
+	case s.chHandleHTTPChannel <- req:
+	case <-req.sc.ctx.Done():
+		return fmt.Errorf("terminated")
+	case <-s.ctx.Done():
+		return fmt.Errorf("terminated")
+	}
+
+	if !req.write {
+		t := time.NewTimer(5 * time.Second)
+		defer t.Stop()
+
+		select {
+		case <-req.res:
+		case <-req.sc.ctx.Done():
+			return fmt.Errorf("terminated")
+		case <-t.C:
+			return fmt.Errorf("did not found a corresponding HTTP POST request")
+		}
+		return errHTTPUpgraded
+	}
+
+	return <-req.res
 }
 
 func (s *Server) handleRequest(req sessionRequestReq) (*base.Response, *ServerSession, error) {

server_conn.go
@@ -1,6 +1,7 @@
 package gortsplib
 
 import (
+	"bufio"
 	"context"
 	"crypto/rand"
 	"crypto/tls"
@@ -15,7 +16,6 @@ import (
 	"github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
 	"github.com/bluenviron/gortsplib/v4/pkg/bytecounter"
-	"github.com/bluenviron/gortsplib/v4/pkg/conn"
 	"github.com/bluenviron/gortsplib/v4/pkg/description"
 	"github.com/bluenviron/gortsplib/v4/pkg/headers"
 	"github.com/bluenviron/gortsplib/v4/pkg/liberrors"
@@ -195,21 +195,25 @@ type readReq struct {
 
 // ServerConn is a server-side RTSP connection.
 type ServerConn struct {
-	s     *Server
-	nconn net.Conn
+	s      *Server
+	nconn  net.Conn
+	isHTTP bool
 
-	ctx        context.Context
-	ctxCancel  func()
-	propsMutex sync.RWMutex
-	userData   interface{}
-	remoteAddr *net.TCPAddr
-	bc         *bytecounter.ByteCounter
-	conn       *conn.Conn
-	session    *ServerSession
-	reader     *serverConnReader
-	authNonce  string
+	ctx              context.Context
+	ctxCancel        func()
+	propsMutex       sync.RWMutex
+	userData         interface{}
+	remoteAddr       *net.TCPAddr
+	bc               *bytecounter.ByteCounter
+	session          *ServerSession
+	reader           *serverConnReader
+	authNonce        string
+	httpReadBuf      *bufio.Reader
+	httpReadTunnelID string
 
 	// in
+	chRequest       chan readReq
+	chReadError     chan error
 	chRemoveSession chan *ServerSession
 
 	// out
@@ -219,7 +223,7 @@ type ServerConn struct {
 func (sc *ServerConn) initialize() {
 	ctx, ctxCancel := context.WithCancel(sc.s.ctx)
 
-	if sc.s.TLSConfig != nil {
+	if sc.s.TLSConfig != nil && !sc.isHTTP {
 		sc.nconn = tls.Server(sc.nconn, sc.s.TLSConfig)
 	}
 
@@ -227,6 +231,8 @@ func (sc *ServerConn) initialize() {
 	sc.ctx = ctx
 	sc.ctxCancel = ctxCancel
 	sc.remoteAddr = sc.nconn.RemoteAddr().(*net.TCPAddr)
+	sc.chRequest = make(chan readReq)
+	sc.chReadError = make(chan error)
 	sc.chRemoveSession = make(chan *ServerSession)
 	sc.done = make(chan struct{})
 
@@ -278,7 +284,14 @@ func (sc *ServerConn) Session() *ServerSession {
 
 // Transport returns transport details.
 func (sc *ServerConn) Transport() *ConnTransport {
-	return &ConnTransport{}
+	return &ConnTransport{
+		Tunnel: func() Tunnel {
+			if sc.isHTTP {
+				return TunnelHTTP
+			}
+			return TunnelNone
+		}(),
+	}
 }
 
 // Stats returns connection statistics.
@@ -349,7 +362,6 @@ func (sc *ServerConn) run() {
 		})
 	}
 
-	sc.conn = conn.NewConn(sc.bc)
 	sc.reader = &serverConnReader{
 		sc: sc,
 	}
@@ -359,7 +371,9 @@ func (sc *ServerConn) run() {
 
 	sc.ctxCancel()
 
-	sc.nconn.Close()
+	if !errors.Is(err, errHTTPUpgraded) {
+		sc.nconn.Close()
+	}
 
 	if sc.reader != nil {
 		sc.reader.wait()
@@ -382,10 +396,10 @@ func (sc *ServerConn) run() {
 func (sc *ServerConn) runInner() error {
 	for {
 		select {
-		case req := <-sc.reader.chRequest:
+		case req := <-sc.chRequest:
 			req.res <- sc.handleRequestOuter(req.req)
 
-		case err := <-sc.reader.chError:
+		case err := <-sc.chReadError:
 			sc.reader = nil
 			return err
 
@@ -615,7 +629,7 @@ func (sc *ServerConn) handleRequestOuter(req *base.Request) error {
 	}
 
 	sc.nconn.SetWriteDeadline(time.Now().Add(sc.s.WriteTimeout))
-	err2 := sc.conn.WriteResponse(res)
+	err2 := sc.reader.conn.WriteResponse(res)
 	if err == nil && err2 != nil {
 		err = err2
 	}

commit 26816c696a8765fcaf2464c1c8ca9a2db6e4347a

server: fix panic due to regression in #887
This happened when writing a TCP packet to a conn after a read error.

client.go
@@ -882,6 +882,7 @@ func (c *Client) runInner() error {
 			}
 
 		case err := <-c.chReadError:
+			c.reader.close()
 			c.reader = nil
 			return err
 
@@ -918,6 +919,7 @@ func (c *Client) waitResponse(requestCseqStr string) (*base.Response, error) {
 			}
 
 		case err := <-c.chReadError:
+			c.reader.close()
 			c.reader = nil
 			return nil, err
 
@@ -975,7 +977,7 @@ func (c *Client) doClose() {
 
 	if c.reader != nil {
 		c.nconn.Close()
-		c.reader.wait()
+		c.reader.close()
 		c.reader = nil
 		c.nconn = nil
 		c.conn = nil

client_reader.go
@@ -12,9 +12,16 @@ type clientReader struct {
 
 	mutex                  sync.Mutex
 	allowInterleavedFrames bool
+
+	terminate chan struct{}
+
+	done chan struct{}
 }
 
 func (r *clientReader) start() {
+	r.terminate = make(chan struct{})
+	r.done = make(chan struct{})
+
 	go r.run()
 }
 
@@ -24,19 +31,20 @@ func (r *clientReader) setAllowInterleavedFrames(v bool) {
 	r.allowInterleavedFrames = v
 }
 
-func (r *clientReader) wait() {
-	for {
-		select {
-		case <-r.c.chResponse:
-		case <-r.c.chRequest:
-		case <-r.c.chReadError:
-			return
-		}
-	}
+func (r *clientReader) close() {
+	close(r.terminate)
+	<-r.done
 }
 
 func (r *clientReader) run() {
-	r.c.chReadError <- r.runInner()
+	defer close(r.done)
+
+	err := r.runInner()
+
+	select {
+	case r.c.chReadError <- err:
+	case <-r.terminate:
+	}
 }
 
 func (r *clientReader) runInner() error {
@@ -48,10 +56,16 @@ func (r *clientReader) runInner() error {
 
 		switch what := what.(type) {
 		case *base.Response:
-			r.c.chResponse <- what
+			select {
+			case r.c.chResponse <- what:
+			case <-r.terminate:
+			}
 
 		case *base.Request:
-			r.c.chRequest <- what
+			select {
+			case r.c.chRequest <- what:
+			case <-r.terminate:
+			}
 
 		case *base.InterleavedFrame:
 			r.mutex.Lock()

server.go
@@ -401,6 +401,9 @@ func (s *Server) runInner() error {
 			sc.Close()
 
 		case req := <-s.chHandleHTTPChannel:
+			if _, ok := s.conns[req.sc]; !ok {
+				continue
+			}
 			if !req.write {
 				req.sc.httpReadTunnelID = req.tunnelID
 				s.httpReadChannels[req.sc] = req.res
@@ -551,8 +554,6 @@ func (s *Server) handleHTTPChannel(req sessionHandleHTTPChannelReq) error {
 
 	select {
 	case s.chHandleHTTPChannel <- req:
-	case <-req.sc.ctx.Done():
-		return fmt.Errorf("terminated")
 	case <-s.ctx.Done():
 		return fmt.Errorf("terminated")
 	}

server_conn.go
@@ -16,6 +16,7 @@ import (
 	"github.com/bluenviron/gortsplib/v4/pkg/auth"
 	"github.com/bluenviron/gortsplib/v4/pkg/base"
 	"github.com/bluenviron/gortsplib/v4/pkg/bytecounter"
+	"github.com/bluenviron/gortsplib/v4/pkg/conn"
 	"github.com/bluenviron/gortsplib/v4/pkg/description"
 	"github.com/bluenviron/gortsplib/v4/pkg/headers"
 	"github.com/bluenviron/gortsplib/v4/pkg/liberrors"
@@ -205,8 +206,8 @@ type ServerConn struct {
 	userData         interface{}
 	remoteAddr       *net.TCPAddr
 	bc               *bytecounter.ByteCounter
+	conn             *conn.Conn
 	session          *ServerSession
-	reader           *serverConnReader
 	authNonce        string
 	httpReadBuf      *bufio.Reader
 	httpReadTunnelID string
@@ -362,10 +363,10 @@ func (sc *ServerConn) run() {
 		})
 	}
 
-	sc.reader = &serverConnReader{
+	reader := &serverConnReader{
 		sc: sc,
 	}
-	sc.reader.initialize()
+	reader.initialize()
 
 	err := sc.runInner()
 
@@ -375,9 +376,7 @@ func (sc *ServerConn) run() {
 		sc.nconn.Close()
 	}
 
-	if sc.reader != nil {
-		sc.reader.wait()
-	}
+	reader.wait()
 
 	if sc.session != nil {
 		sc.session.removeConn(sc)
@@ -400,7 +399,6 @@ func (sc *ServerConn) runInner() error {
 			req.res <- sc.handleRequestOuter(req.req)
 
 		case err := <-sc.chReadError:
-			sc.reader = nil
 			return err
 
 		case ss := <-sc.chRemoveSession:
@@ -629,7 +627,7 @@ func (sc *ServerConn) handleRequestOuter(req *base.Request) error {
 	}
 
 	sc.nconn.SetWriteDeadline(time.Now().Add(sc.s.WriteTimeout))
-	err2 := sc.reader.conn.WriteResponse(res)
+	err2 := sc.conn.WriteResponse(res)
 	if err == nil && err2 != nil {
 		err = err2
 	}

server_conn_reader.go
@@ -36,28 +36,30 @@ func isSwitchReadFuncError(err error) bool {
 }
 
 type serverConnReader struct {
-	sc   *ServerConn
-	conn *conn.Conn
+	sc *ServerConn
+
+	done chan struct{}
 }
 
 func (cr *serverConnReader) initialize() {
+	cr.done = make(chan struct{})
+
 	go cr.run()
 }
 
 func (cr *serverConnReader) wait() {
-	for {
-		select {
-		case <-cr.sc.chReadError:
-			return
-
-		case req := <-cr.sc.chRequest:
-			req.res <- fmt.Errorf("terminated")
-		}
-	}
+	<-cr.done
 }
 
 func (cr *serverConnReader) run() {
-	cr.sc.chReadError <- cr.runInner()
+	defer close(cr.done)
+
+	err := cr.runInner()
+
+	select {
+	case cr.sc.chReadError <- err:
+	case <-cr.sc.ctx.Done():
+	}
 }
 
 func (cr *serverConnReader) runInner() error {
@@ -71,7 +73,7 @@ func (cr *serverConnReader) runInner() error {
 		}
 	}
 
-	cr.conn = conn.NewConn(bufio.NewReader(rw), rw)
+	cr.sc.conn = conn.NewConn(bufio.NewReader(rw), rw)
 
 	readFunc := cr.readFuncStandard
 
@@ -171,7 +173,7 @@ func (cr *serverConnReader) readFuncStandard() error {
 	cr.sc.nconn.SetReadDeadline(time.Time{})
 
 	for {
-		what, err := cr.conn.Read()
+		what, err := cr.sc.conn.Read()
 		if err != nil {
 			return err
 		}
@@ -180,7 +182,12 @@ func (cr *serverConnReader) readFuncStandard() error {
 		case *base.Request:
 			cres := make(chan error)
 			req := readReq{req: what, res: cres}
-			cr.sc.chRequest <- req
+
+			select {
+			case cr.sc.chRequest <- req:
+			case <-cr.sc.ctx.Done():
+				return fmt.Errorf("terminated")
+			}
 
 			err = <-cres
 			if err != nil {
@@ -207,7 +214,7 @@ func (cr *serverConnReader) readFuncTCP() error {
 			cr.sc.nconn.SetReadDeadline(time.Now().Add(cr.sc.s.ReadTimeout))
 		}
 
-		what, err := cr.conn.Read()
+		what, err := cr.sc.conn.Read()
 		if err != nil {
 			return err
 		}
@@ -216,7 +223,12 @@ func (cr *serverConnReader) readFuncTCP() error {
 		case *base.Request:
 			cres := make(chan error)
 			req := readReq{req: what, res: cres}
-			cr.sc.chRequest <- req
+
+			select {
+			case cr.sc.chRequest <- req:
+			case <-cr.sc.ctx.Done():
+				return fmt.Errorf("terminated")
+			}
 
 			err = <-cres
 			if err != nil {

server_session_format.go
@@ -179,7 +179,7 @@ func (sf *serverSessionFormat) writePacketRTPInQueueTCP(payload []byte) error {
 	sf.sm.ss.tcpFrame.Channel = sf.sm.tcpChannel
 	sf.sm.ss.tcpFrame.Payload = payload
 	sf.sm.ss.tcpConn.nconn.SetWriteDeadline(time.Now().Add(sf.sm.ss.s.WriteTimeout))
-	err := sf.sm.ss.tcpConn.reader.conn.WriteInterleavedFrame(sf.sm.ss.tcpFrame, sf.sm.ss.tcpBuffer)
+	err := sf.sm.ss.tcpConn.conn.WriteInterleavedFrame(sf.sm.ss.tcpFrame, sf.sm.ss.tcpBuffer)
 	if err != nil {
 		return err
 	}

server_session_media.go
@@ -483,7 +483,7 @@ func (sm *serverSessionMedia) writePacketRTCPInQueueTCP(payload []byte) error {
 	sm.ss.tcpFrame.Channel = sm.tcpChannel + 1
 	sm.ss.tcpFrame.Payload = payload
 	sm.ss.tcpConn.nconn.SetWriteDeadline(time.Now().Add(sm.ss.s.WriteTimeout))
-	err := sm.ss.tcpConn.reader.conn.WriteInterleavedFrame(sm.ss.tcpFrame, sm.ss.tcpBuffer)
+	err := sm.ss.tcpConn.conn.WriteInterleavedFrame(sm.ss.tcpFrame, sm.ss.tcpBuffer)
 	if err != nil {
 		return err
 	}