bb82ddcc6d8f21c8c06f31ac2ad228fadeb4f2a8
aler9 <46489434+aler9@users.noreply.github.com>
2025-10-26 10:51:50 +0100

    add readbuffer.SetReadBuffer and use it

--- pkg/readbuffer/read_buffer.go
+++ pkg/readbuffer/read_buffer.go
@@ -1,7 +1,8 @@
-// Package readbuffer contains a function to get the read buffer size of a socket.
+// Package readbuffer contains a function to set the read buffer size of a UDP socket.
 package readbuffer
 
 import (
+	"fmt"
 	"net"
 	"syscall"
 )
@@ -10,4 +11,25 @@ import (
 type PacketConn interface {
 	net.PacketConn
 	SyscallConn() (syscall.RawConn, error)
+	SetReadBuffer(bytes int) error
+}
+
+// SetReadBuffer sets the read buffer size of the UDP connection and checks that it was set correctly.
+func SetReadBuffer(pc PacketConn, size int) error {
+	err := pc.SetReadBuffer(size)
+	if err != nil {
+		return err
+	}
+
+	v, err := ReadBuffer(pc)
+	if err != nil {
+		return err
+	}
+
+	if v != size {
+		return fmt.Errorf("unable to set read buffer size to %v, check that the operating system allows that",
+			size)
+	}
+
+	return nil
 }
