2b842ab5f378ad533cdc456b34fb52df5ac9f246
Alessandro Ros <aler9.dev@gmail.com>
2026-09-05 11:19:57 +0200

    add prettier (#1159)

--- scripts/format.mk
+++ scripts/format.mk
@@ -1,5 +1,18 @@
-format:
+define DOCKERFILE_PRETTIER
+FROM $(NODE_IMAGE)
+RUN yarn global add prettier@3.6.2
+endef
+export DOCKERFILE_PRETTIER
+
+format-go:
 	docker run --rm -v $(shell pwd):/app -w /app \
 	-e CGO_ENABLED=0 \
 	$(LINT_IMAGE) \
 	golangci-lint fmt
+
+format-other:
+	echo "$$DOCKERFILE_PRETTIER" | docker build . -f - -t temp
+	docker run --rm -v "$(shell pwd)/:/s" -w /s temp \
+	sh -c "prettier --write ."
+
+format: format-go format-other
